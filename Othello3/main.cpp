b71a1f5c426ddd03ae5de48cf94531f6f34c40d7
Patty <L224749391@gmail.com>
2021-06-09 20:04:01 +0800

    Initial Commit

---
+++ Othello3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Othello3
+//
+//  Created by 林湘羚 on 2021/6/9.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
